40f3500a60f9a5b05e5fe5e355c87775de29cbcf
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2021-06-06 16:47:39 +0100

    Add more log

--- consumer.go
+++ consumer.go
@@ -99,7 +99,7 @@ func (consumer *Consumer) Consume(delivery rmq.Delivery) {
 	
 	if event.WaitUntil != nil {
 		if time.Now().UTC().Before(*event.WaitUntil) {
-			fmt.Println("REQUEUING")
+			fmt.Println("REQUEUING", *event.WaitUntil)
 			// Requeue event
 			// Emit same event
 			taskBytes, err := json.Marshal(event)
